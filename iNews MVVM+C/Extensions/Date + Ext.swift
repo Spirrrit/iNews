7fb12159bfb6df6bbd2f5afc011530cfdf333361
test <vasiliev.asp@yandx.ru>
2024-08-09 18:25:31 +0300

    готово за исключением правок UI

--- iNews MVVM+C/Extensions/Date + Ext.swift
+++ iNews MVVM+C/Extensions/Date + Ext.swift
@@ -2,16 +2,7 @@
 //  Date + Ext.swift
 //  iNews MVVM+C
 //
-//  Created by Слава on 22.07.2024.
+//  Created by Слава on 09.08.2024.
 //
 
 import Foundation
-
-extension Date {
-    var toRusString: String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "ru_RU")
-        dateFormatter.setLocalizedDateFormatFromTemplate("E, dd MMMM HH:mm")
-        return dateFormatter.string(from: self)
-    }
-}
---
+++ iNews MVVM+C/Services/Services.swift
@@ -0,0 +1,19 @@
+//
+//  AppDependency.swift
+//  iNews MVVM+C
+//
+//  Created by Слава on 21.07.2024.
+//
+
+import Foundation
+
+//Сервис по работе с сетью и базой данных
+
+struct Services : NetworkServicesProtocol, CoreDataServiceProtocol {
+    var networkService: NetworkService
+    var coreDataService: CoreDataService
+}
+
+
+
+
